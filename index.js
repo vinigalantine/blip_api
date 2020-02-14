a9586d6b18cecffc2e1512aac19731fd3dc60da9
Vinícius Leal Galantine <vinicius.galantine@outlook.com>
2020-02-14 01:14:50 -0300

    I tried
    I tried my best, "learned to use" node.js today with 2 hours... Surprised but not disappointed hahahaha

--- index.js
+++ index.js
@@ -15,56 +15,128 @@ let ACCESS_KEY = 'elRUM2tzZzVzN2FaaG9RTnVsc3I=';
 // Criando as configurações de conexão
 // Create connection configs
 let client = new BlipSdk.ClientBuilder()
-						.withIdentifier(IDENTIFIER)
-						.withAccessKey(ACCESS_KEY)
-						.withTransportFactory(() => new WebSocketTransport())
-						.build();
+                        .withIdentifier(IDENTIFIER)
+                        .withAccessKey(ACCESS_KEY)
+                        .withTransportFactory(() => new WebSocketTransport())
+                        .build();
 
 client.connect() // This method returns a 'promise'
-	.then(function(session) {
-    	// Conexão OK!
-    	// Connected!
-    	// Este código foi apenas o primeiro protótipo, alguma coisa eu tinha q enregar ----------
-    	client.addMessageReceiver(true, function(message) {
-    		if(message.type == 'text/plain'){
-    			if(message.content.toLowerCase() == "c#"){
-    				var resp = "";
-    				octokit.repos.listForOrg({
-    					org: "takenet",
-    					type: "public"
-    				}).then(({ data }) => {
-    					let count = 0;
-    					data.forEach(function(val,idx){
-    						if(val.language == "C#"){
-    							if(count++ < 5){
-    								resp += "<b>Repo #"+count+"</b> \n Nome:"+val.name+"\n Data de criação: "+val.created_at + "\n Linguagem: "+val.language + "\n";
-    							}
-    							if(count == 5){
-    								var msg = {
-    									type: "text/plain",
-    									content: resp,
-    									to: message.from
-    								};
-    								client.sendMessage(msg);
-    							}
-    						}
-    					});    
-    				});                       
-    			} else{
-    				var msg = {
-    					type: "text/plain",
-    					content: "Desculpa, não entendi! Digite \"C#\" para a listagem de repositórios :(",
-    					to: message.from
-    				};
-    				console.log(msg);
-    				client.sendMessage(msg);
-    			}
-    		}
-    	});
-    	// Final do trecho a se melhorar -----------------------------------------------------
+    .then(function(session) {
+        // Conexão OK!
+        // Connected!
+        console.log("Connected!");
+        let welcome_count = 0;
+        client.addMessageReceiver(true, function(message) {
+            if(welcome_count < 1 && message.content.state == "composing"){
+                welcome_count++;
+                client.sendMessage({
+                  type: "application/vnd.lime.select+json",
+                  to: message.from,
+                  content: {
+                    scope:"immediate",
+                    text: "Olá! Vamos começar?",
+                    options: [
+                        {
+                            text: "Sim"
+                        },
+                        {
+                            text: "Não"
+                        }
+                    ]
+                  }
+                });
+            } else if (message.type == 'text/plain' && message.content.toLowerCase() == "sim") {
+                var rspnse = {
+                    type: "application/vnd.lime.collection+json",
+                    to: message.from,
+                    content: {
+                        itemType: "application/vnd.lime.document-select+json",
+                        items: []
+                    }
+                }
+                octokit.repos.listForOrg({
+                    org: "takenet",
+                    type: "public"
+                }).then(({ data }) => {
+                    let count = 0;
+                    data.forEach(function(val,idx){
+                        if(val.language == "C#"){
+                            if(count++ < 5){
+                                rspnse.content.items.push({
+                                    header: {
+                                        type: "application/vnd.lime.media-link+json",
+                                        value: {
+                                            title: val.name,
+                                            text: val.description == null ? "" : val.description,
+                                            type: "image/jpeg",
+                                            uri: "https://avatars1.githubusercontent.com/u/4369522?s=200&v=4"
+                                        }
+                                    }
+                                });
+                                if(count == 5){
+                                    client.sendMessage(rspnse);
+                                }
+                            }
+                        }
+                        });
+                    });
+            } else if (message.content.state == "paused"){
+                setTimeout(function(){ 
+                    client.sendMessage({
+                       type: "text/plain",
+                       content: "Esperando sua resposta... rs ;)",
+                       to: message.from
+                    }); 
+                }, 3000);
+                setTimeout(function(){
+                    client.sendMessage({
+                      type: "application/vnd.lime.select+json",
+                      to: message.from,
+                      content: {
+                            scope:"immediate",
+                            text: "Deseja listar mais uma vez?",
+                            options: [
+                            {
+                                text: "Sim"
+                            },
+                            {
+                                text: "Não"
+                            }
+                            ]
+                        }
+                    });
+                }, 6000);
+            } else if (message.type == 'text/plain' && message.content.toLowerCase() == "não") {
+                let msg = {
+                     type: "text/plain",
+                     content: "Poxa... Tudo bem ;-;",
+                     to: message.from
+                };
+                client.sendMessage(msg);
+            } else if (message.type == 'text/plain') {
+                setTimeout(function(){
+                    client.sendMessage({
+                      type: "application/vnd.lime.select+json",
+                      to: message.from,
+                      content: {
+                            scope:"immediate",
+                            text: "Desculpa, não entendi! Deseja listar mais uma vez?",
+                            options: [
+                            {
+                                text: "Sim"
+                            },
+                            {
+                                text: "Não"
+                            }
+                            ]
+                        }
+                    });
+                }, 3000);
+            } 
+        });
     })
-	.catch(function(err) { 
-    	// Pega o erro e printa
-    	// Catch error
-    	console.log(err); 
+    .catch(function(err) { 
+        // Pega o erro e printa
+        // Catch error
+        console.log(err); 
     });
